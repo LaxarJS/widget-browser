995b87d593166e66aaeb5a17b2efcc51d14ea7b7
Jonas Schulte <jonas.schulte@aixigo.de>
2015-03-03 11:23:45 +0100

    gh-pages for release v0.1.0

---
+++ var/static/portal_angular_dependencies.js
@@ -0,0 +1,14 @@
+define( [
+   '../includes/widgets/widget-browser/ax-widget-listing-activity/ax-widget-listing-activity',
+   '../includes/widgets/laxarjs/ax-messages-widget/ax-messages-widget',
+   '../includes/widgets/widget-browser/ax-widget-browser-widget/ax-widget-browser-widget',
+   '../includes/widgets/laxarjs/ax-markdown-display-widget/ax-markdown-display-widget',
+   '../includes/widgets/widget-browser/ax-artifact-repository-link-widget/ax-artifact-repository-link-widget',
+   '../includes/widgets/widget-browser/ax-widget-information-widget/ax-widget-information-widget',
+   '../includes/widgets/laxarjs/ax-media-widget/ax-media-widget',
+   'laxar_uikit/controls/i18n'
+], function() {
+   'use strict';
+
+   return [].map.call( arguments, function( module ) { return module.name; } );
+} );
